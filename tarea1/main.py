4901470c3ac931756d9d456c1ab56d32d7b0fea2
renato <renatocespedesf@gmail.com>
2022-11-02 17:23:35 -0500

    archivo main.py

---
+++ tarea1/main.py
@@ -0,0 +1,13 @@
+import csv
+lista_libros=[]
+class Libro:
+    def __init__(self):
+        
+        self.__id=0
+        self.__titulo=""
+        self.__genero=""
+        self.__id_ISBN=""
+        self.__editorial=""
+        self.__autores=[]
+	
+
