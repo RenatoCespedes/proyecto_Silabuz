e580a4bdc75181852f5a05202efba42863228c44
JhohanJC <111875@unsaac.edu.pe>
2022-11-04 02:22:44 -0500

    Opcion 2,4 -> archivo tarea2

--- tarea2/main.py
+++ tarea2/main.py
@@ -18,7 +18,23 @@ data=data.json()
 pokemons_in_generation=[i['name'] for i in data['pokemon_species']]
 print_list(pokemons_in_generation)
 
-
+# Opción 2: Listar pokemons por forma. Se ingresa alguna forma (deben sugerir valores)
+# y se listan todos los pokemons respectivos.
+print('\n Cargando datos opción 2 ...')
+a_form={}
+test_form=True
+limite=400 # maximo->limite=906
+for n_form in range(200,limite):#posibles valores a encontrar
+    pokemon_form=f'https://pokeapi.co/api/v2/pokemon-form/{n_form}/'
+    resp_form = requests.get(pokemon_form)
+    dato_form=resp_form.json()
+    if dato_form['form_name'] != '':
+        name_poke=dato_form['pokemon']['name']
+        a={dato_form['form_name']:name_poke}
+        a_form.update(a)
+print(f"Ejemplo de formas: {a_form.keys()}")
+form_in=input("Ingrese una forma: ")
+print("Pokemon: ", a_form[form_in])
 
 #Opción 3: Listar pokemons por habilidad. Se deben sugerir opciones a 
 # ingresar para interactuar.
@@ -36,6 +52,25 @@ if ability in list_of_abilities:
     pokemons=[i['pokemon']['name'] for i in list_data_pokemon]
     print_list(pokemons)
 
+#Opción 4: Listar pokemons por habitat. 
+# Se deben sugerir opciones a ingresar para interactuar.
+print("Opción 4")
+opcion=True
+habitat_pokemon={}
+nm=1
+while opcion:
+    pokemon_habitat=f'https://pokeapi.co/api/v2/pokemon-habitat/{nm}/'
+    resp_hab = requests.get(pokemon_habitat)
+    nm+=1
+    if f'{resp_hab}'=='<Response [200]>':
+        dato_hab=resp_hab.json()
+        a_hab={dato_hab['name']:[k['name'] for k in dato_hab["pokemon_species"]]}
+        habitat_pokemon.update(a_hab)
+    else:
+      opcion=False
+print('Ejemplos de habitat: cave, forest, grassland, etc. ')
+habitat_in=input('Ingrese un habitat: ')
+print('Los pokemons son: ', ', '.join(habitat_pokemon[f'{habitat_in}']))
 
 
 
